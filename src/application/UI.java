2c3275198a26cbbe169de580269c1f5791003aef
Rafael Niccioli <r.niccioli@gmail.com>
2025-09-23 23:51:28 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,6 +4,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -59,9 +60,14 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()){
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheck()){
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()){
+                System.out.println("CHECK!");
+            }
+        } else {
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
         }
     }
 
